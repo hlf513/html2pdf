223fd586580c55f156a3025d626982dba8626db1
helongfei <helongfei@helongfeideMacBook-Pro.local>
2022-09-16 01:13:44 +0800

    update generateOutline.js

--- generateOutline.js
+++ generateOutline.js
@@ -17,7 +17,7 @@ async function main() {
             continue
         }
         const path = process.argv[2]+'/'+item
-        const filename = path.split('/').pop().replace('.pdf','').replace("【daobanke.com】",'')
+        const filename = path.split('/').pop().replace('.pdf','').replace("something",'')
         // .replace(/\d{2}/,'')
         const pdf = await PDFDocument.load(fs.readFileSync(path))
         console.log(filename+" "+page)
